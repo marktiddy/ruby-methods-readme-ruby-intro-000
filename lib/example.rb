1a11063de6c2aa4b3386f3e36cc029dd7ece4547
Mark Tiddy <hello@marktiddy.co.uk>
2019-09-17 15:39:28 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,7 +1,7 @@
 # Use this space to code along with the readme
 def say_hello_world_ten_times
   phrase = "Hello World"
-  10.times puts phrase
+  10.times puts "Hello World"
 end
 
 say_hello_world_ten_times
